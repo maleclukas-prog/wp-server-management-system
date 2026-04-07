923ca415d97c7d6cbbc6f21a8b24592a0f1d6298
Lukasz Malec <maleclukas@gmail.com>
2026-04-07 15:48:17 +0100

    Update wp-help.sh

--- scripts/wp-help.sh
+++ scripts/wp-help.sh
@@ -6,7 +6,6 @@
 # =================================================================
 source $HOME/scripts/wsms-config.sh
 
-BLUE='\033[0;34m'
 # Professional UI Colors
 RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[1;33m'; BLUE='\033[0;34m'
 PURPLE='\033[0;35m'; CYAN='\033[0;36m'; WHITE='\033[1;37m'; NC='\033[0m'
